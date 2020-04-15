574db5cd2db55fe76d1c25f254ed93bbf64ae7ac
Len Chang <lamgo.chang@gmail.com>
2020-04-15 16:06:00 +0800

    Modern React with Redux: Section 13 - 143 to 149 Initialize the Redux App

---
+++ src/reducers/index.js
@@ -0,0 +1,15 @@
+const songsReducer = () => {
+  return [
+    { title: "Top 100 songs of the 1990s", duration: "14:50" },
+    { title: "365", duration: "4:23" },
+    { title: "I Don't Care", duration: "4:44" },
+  ];
+};
+
+const selectedSongReducer = (selectedSong = null, action) => {
+  if (action.type === "SONG_SELECTED") {
+    return action.payload;
+  }
+
+  return selectedSong;
+};
